5722c95450981056c355c3172a7b56c5cd6dc4e2
cellmate <cellmate@protonemail.com>
2022-01-14 04:35:35 +1100

    test - main app component

--- coding-challenge-ui/src/components/App/App.test.tsx
+++ coding-challenge-ui/src/components/App/App.test.tsx
@@ -1,15 +1,28 @@
-import App from './App';
-import React from 'react';
-import { render } from '@testing-library/react';
 
-test('renders app header', () => {
+import { render} from '@testing-library/react'
+import App from './App';
+import { shallow } from 'enzyme';
+import toJSON from "enzyme-to-json";
+
+test("has the main component loaded", () => {
+  const wrapper = shallow(<App />);
+  expect(toJSON(wrapper)).toMatchSnapshot()
+});
+
+test("has OverdueOrderTable component loaded", () => {
+  const wrapper = shallow(<App />);
+  expect(toJSON(wrapper)).toMatchSnapshot()
+  expect(wrapper.text().includes("OverdueOrderTable"))
+});
+
+test('renders app title', () => {
   const { getByText } = render(<App />);
   const headerText = getByText("Analytics Dashboard");
-  expect(headerText).toBeInTheDocument();
+  expect(headerText)
 });
 
 test('renders user name', () => {
   const { getByText } = render(<App />);
   const headerText = getByText("Welcome, Guest!");
-  expect(headerText).toBeInTheDocument();
+  expect(headerText)
 });
