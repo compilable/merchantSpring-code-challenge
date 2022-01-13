913bff631cf0065b0aaeabe32685ce2ffc30ce1a
cellmate <cellmate@protonemail.com>
2022-01-14 00:19:07 +1100

    error handler for fetch

--- coding-challenge-ui/src/components/App/AppLogic.tsx
+++ coding-challenge-ui/src/components/App/AppLogic.tsx
@@ -1,38 +1,50 @@
-import React,{ useState } from "react"
+import React, { useState } from "react"
 import { User } from "./User"
 import { OverdueOrder } from "../OverdueOrderTable/OverdueOrder"
-const API_ENDPOINT="http://localhost:8080/"
+const API_ENDPOINT = "http://localhost:8080/"
 
 
 const AppLogic = () => {
 
-    const [user, setUser] = useState<User | null>(null);
-    const [overdueOrders, _setOverdueOrders] = useState<Array<OverdueOrder>>([]);
+  const [user, setUser] = useState<User | null>(null);
+  const [overdueOrders, _setOverdueOrders] = useState<Array<OverdueOrder>>([]);
 
-    const setOverdueOrders = (overdueOrders: Array<OverdueOrder>) => {
-        overdueOrders.map((overdueOrder) => {
-          return(overdueOrder.country = overdueOrder.country.toLowerCase().substring(0, 2))
-        });
-        _setOverdueOrders(overdueOrders)
-    };
+  const setOverdueOrders = (overdueOrders: Array<OverdueOrder>) => {
+    overdueOrders.map((overdueOrder) => {
+      return (overdueOrder.country = overdueOrder.country.toLowerCase().substring(0, 2))
+    });
+    _setOverdueOrders(overdueOrders)
+  };
 
-    React.useEffect(() => {
-        fetch(API_ENDPOINT+"user")
-          .then((results) => results.json())
-          .then((data) => {
-            setUser(data);
-          });
-    
-        fetch(API_ENDPOINT+"overdueOrders")
-          .then((results) => results.json())
-          .then((data) => {
-            setOverdueOrders(data);
-          });
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-      }, []);
-    
+  const logError = (error:any)=>{
+    console.error(`unable to load the data from the server, please check the connection to : ${API_ENDPOINT}`)
+  }
 
-    return{user,overdueOrders,setOverdueOrders,setUser}
+  React.useEffect(() => {
+    fetch(API_ENDPOINT + "user")
+      .then((results) => results.json())
+      .then((data) => {
+        setUser(data);
+      }, (error) => {
+        if (error) {
+          logError(error)
+        }
+      });
+
+    fetch(API_ENDPOINT + "overdueOrders")
+      .then((results) => results.json())
+      .then((data) => {
+        setOverdueOrders(data);
+      }, (error) => {
+        if (error) {
+          logError(error)
+        }
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+
+  return { user, overdueOrders, setOverdueOrders, setUser }
 
 }
 
